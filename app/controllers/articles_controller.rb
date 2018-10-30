c21e5769bc9b86f58330de946d470fdffbc84682
Adam Dawkins <adam@dragondrop.uk>
2018-10-30 02:26:07 +0000

    ♻️  Limit number of articles to 20

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :authorize, only: [:create, :new]
   def index
-    @articles = Article.all
+    @articles = Article.all.limit(20)
   end
 
   def new
